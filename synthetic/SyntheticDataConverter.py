9b8a8cb83f3df0dde6ee16c870b98db4e82f08b8
isaranto <isarand@gmail.com>
2016-09-06 18:47:46 +0300

    [DEV] event extraction completed

--- synthetic/SyntheticDataConverter.py
+++ synthetic/SyntheticDataConverter.py
@@ -15,13 +15,14 @@ class SyntheticDataConverter:
 
         self.edges_files = [item for item in sorted(files) if item.endswith('edges')]
         self.comm_files = [item for item in sorted(files) if item.endswith('comm')]
+        self.event_file = [item for item in sorted(files) if item.endswith('events')]
         self.timeframes = len(self.edges_files)
         self.edges = self.get_edges()
         self.communities = self.get_comms()
+        self.events = self.get_events()
         self.graphs = {}
         for i in range(1, self.timeframes+1):
             self.graphs[int(i)] = nx.Graph(self.edges[i])
-
 
 
     def get_edges(self):
@@ -42,3 +43,19 @@ class SyntheticDataConverter:
                     comms[int(j)] = [int(node) for node in line.split()]
             com_time[int(i)] = comms
         return com_time
+
+    def get_events(self):
+        events = {}
+        for i, e_file in enumerate(self.event_file, 1):
+            with open(self.filepath+e_file, 'r') as fp:
+                for line in fp:
+                    event = {}
+                    e = line.strip().split(',')
+                    event[int(e[2])] = e[1]
+                    try:
+                        events[int(e[0])].append(event)
+                    except KeyError:
+                        events[int(e[0])] = []
+                        events[int(e[0])].append(event)
+        return events
+
