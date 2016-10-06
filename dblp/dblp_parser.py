cdf48c3da56835a56da7dc806ff73e3b46bc02f9
isaranto <isarand@gmail.com>
2016-10-06 17:49:32 +0300

    [DEV] DBLP communities based on connected components

--- dblp/dblp_parser.py
+++ dblp/dblp_parser.py
@@ -51,8 +51,8 @@ class dblp_parser:
         # auth_id = {}
         print len(authors), " # of authors"
         with open("../data/dblp/author_ids.txt", 'w')as fp:
-            for i, author in enumerate(authors, 1):
-                fp.write(str(i)+","+author.encode('utf-8')+"\n")
+            for j, author in enumerate(authors, 1):
+                fp.write(str(j)+","+author.encode('utf-8')+"\n")
         auth_id = {}
         with open("../data/dblp/author_ids.txt", 'r') as fp:
             for line in fp:
@@ -63,8 +63,8 @@ class dblp_parser:
         for year, confs in self.data.items():
             for conf, papers in confs.items():
                 for auth_list in papers:
-                    for i, auth in enumerate(auth_list):
-                        auth_list[i] = self.authors[auth]
+                    for j, auth in enumerate(auth_list):
+                        auth_list[j] = self.authors[auth]
 
     def write_new_file(self):
         with open('my_dblp_data.json', 'w')as fp:
@@ -72,17 +72,16 @@ class dblp_parser:
 
 
 class dblp_loader:
-    def __init__(self, filename, start_year=1959, end_year=2017):
-        with open(filename, 'r')as fp:
+    def __init__(self, _file, start_year=1959, end_year=2017):
+        with open(_file, 'r')as fp:
             # load json and convert year-keys to int
             self.data = {int(k): v for k, v in json.load(fp).items()}
         self.edges = self.get_edges(start_year, end_year)
         self.graphs = self.get_graphs(start_year, end_year)
         self.timeFrames = range(start_year, end_year+1)
-        #self.communities = self.get_comms(start_year, end_year)
-        self.conf_communities = self.test_com(start_year, end_year)
-        # self.cc_communities = self.cc_com(start_year, end_year)
-
+        # self.communities = self.get_comms(start_year, end_year)
+        #self.conf_communities = self.get_conf_com(start_year, end_year)
+        self.cc_communities = self.get_cc_com(start_year, end_year)
 
     def get_edges(self, start_year, end_year):
         edge_time = {}
@@ -96,17 +95,23 @@ class dblp_loader:
         return edge_time
 
     def get_comms(self, start_year, end_year):
+        """
+        this is being used to get all conferences as communities
+        :param start_year:
+        :param end_year:
+        :return:
+        """
         com_time = {}
         for year in range(start_year, end_year+1):
-        # for year, confs in self.data.iteritems():
+            # for year, confs in self.data.iteritems():
             comms = {}
-            for i, (conf, papers) in enumerate(self.data[year].iteritems(), 1):
+            for j, (conf, papers) in enumerate(self.data[year].iteritems(), 1):
                 com = [author for paper in papers for author in paper]
                 # get rid of empty confs
                 if len(com) == 0:
                     continue
                 else:
-                    comms[i] = com
+                    comms[j] = com
             com_time[year] = comms
         return com_time
 
@@ -116,30 +121,52 @@ class dblp_loader:
             graphs[year] = nx.Graph(self.edges[year])
         return graphs
 
-    def test_com(self,start_year, end_year):
+    def get_conf_com(self, start_year, end_year):
+        """
+        Returns the author communities for the conferences specified in confs.txt
+        :param start_year:
+        :param end_year:
+        :return:
+        """
         conf_list = []
         with open('../data/dblp/confs.txt', 'r') as fp:
             for conf in fp:
                 conf_list.append(conf.strip().lower())
         com_time = {}
         for year in range(start_year, end_year+1):
-        # for year, confs in self.data.iteritems():
+            # for year, confs in self.data.iteritems():
             comms = {}
-            for i, conf in enumerate(conf_list, 1):
+            for j, conf in enumerate(conf_list, 1):
                 com = []
                 try:
                     for paper in self.data[year][conf]:
                         for author in paper:
                             com.append(author)
-                    #com = [author for paper in self.data[year][conf] for author in paper]
-                except:
+                    # com = [author for paper in self.data[year][conf] for author in paper]
+                except KeyError:
                     # print(year, conf)
                     pass
                 # get rid of empty confs
                 if len(com) == 0:
                     continue
                 else:
-                    comms[i] = com
+                    comms[j] = com
+            com_time[year] = comms
+        return com_time
+
+    def get_cc_com(self, start_year, end_year):
+        """
+        get communities that correspond to connected components
+        :param start_year:
+        :param end_year:
+        :return:
+        """
+        com_time = {}
+        for year, graph in self.graphs.iteritems():
+            comms = {}
+            for j, com in enumerate(list(nx.connected_components(graph)), 1):
+                if len(com) > 3:
+                    comms[j] = list(com)
             com_time[year] = comms
         return com_time
 
@@ -150,25 +177,24 @@ class dblp_loader:
                 conf_len = 0
                 for paper in papers:
                     conf_len += len(paper)
-                if conf_len>0:
+                if conf_len > 0:
                     try:
                         length[conf][year] = conf_len
                     except KeyError:
-                        length[conf]= {year : conf_len}
-        confs =[]
+                        length[conf] = {year: conf_len}
+        confs = []
         for name, years in length.iteritems():
             parts_list = [parts for year, parts in years.iteritems() if year in range(2000, 2005)]
-            if len(years)> 10 and all(parts_list[i]>=1000 for i in range(len(parts_list))):
+            if len(years) > 10 and all(parts_list[j] >= 1000 for j in range(len(parts_list))):
                     confs.append(name)
         return length
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     filename = "../data/dblp/my_dblp_data.json"
     dblp = dblp_loader(filename, start_year=2000, end_year=2004)
     """stats = dblp.get_stats()
-    pprint.pprint(dblp.data, indent=4, width=2)"""
-    print dblp.graphs
+    pprint.pprint(dblp.data, indent=4, width=2)
     for i, graph in dblp.graphs.iteritems():
         print i, nx.number_connected_components(graph)
+    """
