82b840900b4615e7db4e03c3de3de4d0ad7ff96b
isaranto <isarand@gmail.com>
2016-10-10 17:16:13 +0300

    [DEV] started muturank distributions calculation

--- muturank/Muturank.py
+++ muturank/Muturank.py
@@ -4,45 +4,105 @@ import numpy as np
 import pandas as pd
 
 
+
 class Muturank:
-    def __init__(self, graphs):
-        self.tensor_o, self.tensor_r = self.create_dense_tensors(graphs)
+    def __init__(self, graphs, threshold):
+        self.graphs = graphs
+        self.a, self.o, self.r = self.create_dense_tensors(graphs)
+        self.e = threshold
         # self.tensor= self.create_dense_tensors(graphs)
         # self.frame = self.create_dataframes(self.tensor)
 
     def create_dense_tensors(self, graphs):
         """
-        construct two transition probability tensors O =[O i,j,d] andR =[r i,j,d]
+        construct two transition probability tensors O =[O i,j,d] and R =[r i,j,d]
 
         :param graphs:
         :return:
         """
         n = 0
         # set the number of nodes to the biggest graph found in the timeframes
-        nodes = list(set([node for i in range(len(graphs)) for node in nx.nodes(graphs[i])]))
-        n = len(nodes)
+        self.nodes = list(set([node for i in range(len(graphs)) for node in nx.nodes(graphs[i])]))
+        n = len(self.nodes)
         s = len(graphs)
-        tensor_init = np.zeros((s, n+1, n+1))
-        for i, node in enumerate(nodes, 1):
-            tensor_init[:, i, 0] = node
-            tensor_init[:, 0, i] = node
+        a = np.zeros((s, n+1, n+1))
+        for i, node in enumerate(self.nodes, 1):
+            a[:, i, 0] = node
+            a[:, 0, i] = node
         for i in range(s):
             for u, v in graphs[i].edges_iter():
-                tensor_init[i, u, v] = 1
-                tensor_init[i, v, u] = 1
-        tensor_o = np.copy(tensor_init)
-        tensor_r = np.copy(tensor_init)
-        for t in range(0, tensor_o.shape[0]):
-            for j in range(1, tensor_o.shape[2]):
-                for i in range(1, tensor_o.shape[1]):
-                    if tensor_init[t, 1:, j].sum()!=0:
-                        tensor_o[t, i, j] = tensor_o[t, i, j]/(tensor_init[t, 1:, j].sum())
-        for i in range(1, tensor_r.shape[1]):
-            for j in range(1, tensor_r.shape[2]):
-                for t in range(0, tensor_r.shape[0]):
-                    if tensor_init[:, i, j].sum()!=0:
-                        tensor_r[t, i, j] = tensor_r[t, i, j]/(tensor_init[:, i, j].sum())
-        return tensor_o, tensor_r
+                a[i, u, v] = 1
+                a[i, v, u] = 1
+        o = np.copy(a)
+        r = np.copy(a)
+        for t in range(0, o.shape[0]):
+            for j in range(1, o.shape[2]):
+                for i in range(1, o.shape[1]):
+                    if a[t, 1:, j].sum()!=0:
+                        o[t, i, j] = o[t, i, j]/(a[t, 1:, j].sum())
+        for i in range(1, r.shape[1]):
+            for j in range(1, r.shape[2]):
+                for t in range(0, r.shape[0]):
+                    if a[:, i, j].sum()!=0:
+                        r[t, i, j] = r[t, i, j]/(a[:, i, j].sum())
+        return a, o, r
+
+    def prob(self, i, j):
+        pass
+
+
+    def prob(self, d, j):
+        pass
+
+
+    def run_muturank(self):
+        """
+        Input:
+            A:      the affinity tensor
+            e:      the convergence threshold
+            p*,q*:  two prior distributions
+            a,b:    two balancing parameters
+        Output:
+            p,q:    two equilibrium distributions
+        :return:
+        """
+        t = 0
+        # p_star = prior
+        # p_old
+        # p_new
+
+        # initializing p_star and q_star with random probabilities
+        p_star = np.random.dirichlet(np.ones(len(self.nodes)))
+        q_star = np.random.dirichlet(np.ones(len(self.graphs)))
+        """
+        alternatively we set q_star and q_star equal to 1/n
+        p_star = np.ones(len(self.nodes))/len(self.nodes)
+        q_star = np.ones(len(self.graphs))/len(self.graphs)
+        """
+        p_new= np.ones((len(p_star)))
+        q_new = np.ones((len(q_star)))
+        p_old = np.zeros((len(p_star)))
+        q_old = np.zeros((len(q_star)))
+        # TODO: while ||p(t)-p(t-1)||^2 + ||q(t) - q(t-1||^2 >=e
+        while np.linalg.norm(p_new-p_old)**2 + np.linalg.norm(q_new-q_old)**2 > self.e:
+            p_old = p_new
+            q_old = q_new
+            for i in range(len(self.nodes)):
+                # TODO: calculate p_new
+                p_new[i]= 0
+            for d in range(len(self.graphs)):
+                # TODO: calculate q_new
+                q_new[d] = 0
+            t += 1
+        return p_new, q_new
+
+
+
+        # TODO:     { compute p(t+1) and q(t+1)
+        # TODO:     t = t+1 }
+        # TODO:
+        # TODO:
+
 
     def create_dataframes(self, tensor):
         dataframes = {}
@@ -53,9 +113,9 @@ class Muturank:
             columns=tensor[0, 1:])
 
     def create_sp_tensors(self, graphs):
-        tensor_o = None
-        tensor_r = None
-        return tensor_o, tensor_r
+        o = None
+        r = None
+        return o, r
 
 
 
@@ -73,4 +133,6 @@ if __name__ == '__main__':
     graphs = {}
     for i, edges in edges.items():
         graphs[i] = nx.Graph(edges)
-    print Muturank(graphs).tensor_o
+    print Muturank(graphs).o
+    print Muturank(graphs).r
+
