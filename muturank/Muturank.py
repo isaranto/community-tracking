[FIX] added two dimentional sum arrays to be used for prob computation

muturank/Muturank.py
@@ -32,32 +32,28 @@ class Muturank:
                 tuples.append([self.node_pos[u], self.node_pos[v], i])
                 tuples.append([self.node_pos[v], self.node_pos[u], i])
         triplets = np.array([(u, v, t) for u, v, t in tuples])
-        np.ones(len(triplets))
         a = sptensor(tuple(triplets.T), vals=np.ones(len(triplets)), shape=(len(self.node_ids),
                                                                             len(self.node_ids),
-                                                                          len(graphs)))
+                                                                            len(graphs)))
         o_values = []
+        sum_rows = np.zeros((a.shape[0], a.shape[2]))
         for t in range(a.shape[2]):
-            sum = []
             for i in range(a.shape[0]):
                 for j in range(a.shape[1]):
                     # TODO : just add another for loop instead of : to access .sum()
                     # TODO : check sparse tensor performance and library
-                    try:
-                        sum[i] += a[i, j, t]
-                    except IndexError:
-                        sum.append(a[i, j, t])
+                    sum_rows[i, t] += a[i, j, t]
             for i in range(a.shape[0]):
-                if sum[i] != 0:
+                if sum_rows[i, t] != 0:
                     for j in range(i):
                         if a[i, j, t] != 0:
-                            o_values.append(a[j, i, t]/sum[j])
-                            o_values.append(a[i, j, t]/sum[i])
+                            o_values.append(a[j, i, t]/sum_rows[j, t])
+                            o_values.append(a[i, j, t]/sum_rows[i, t])
         o = sptensor(tuple(triplets.T), vals=o_values, shape=(len(self.node_ids),
                                                                             len(self.node_ids),
                                                                           len(graphs)))
         r_values = []
-        sum = np.zeros((a.shape[0], a.shape[1]))
+        sum_time = np.zeros((a.shape[0], a.shape[1]))
         for i in range(a.shape[0]):
             # TODO: sum is a dense matrix/array. Should be sparse for memory
             for j in range(a.shape[1]):
@@ -65,18 +61,17 @@ class Muturank:
                     # TODO : just add another for loop instead of : to access .sum()
                     # TODO : check sparse tensor performance and library
                     if a[i, j, t] != 0:
-                        sum[i, j] += a[i, j, t]
+                        sum_time[i, j] += a[i, j, t]
         for t in range(a.shape[2]):
             for i in range(a.shape[0]):
                 for j in range(i):
                     if a[j, i, t] != 0:
-                        r_values.append(a[j, i, t]/sum[j, i])
-                        r_values.append(a[i, j, t]/sum[i, j])
+                        r_values.append(a[j, i, t]/sum_time[j, i])
+                        r_values.append(a[i, j, t]/sum_time[i, j])
         r = sptensor(tuple(triplets.T), vals=r_values, shape=(len(self.node_ids),
                                                                             len(self.node_ids),
                                                                           len(graphs)))
-
-        return a, o, r
+        return a, o, r, sum_rows, sum_time
 
     def create_dtensors(self, graphs):
         """