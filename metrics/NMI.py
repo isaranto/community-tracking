8cefec6e97ade727a0c145491196c78f2b225f84
isaranto <isarand@gmail.com>
2017-01-19 17:57:26 +0200

    [DEV][metrics] added nmi

--- metrics/NMI.py
+++ metrics/NMI.py
@@ -1,13 +1,34 @@
 from __future__ import division
 from itertools import combinations
 from collections import Counter
+from subprocess import Popen, PIPE
+
+
+
 
 class NMI:
     def __init__(self, comms1, comms2):
-        self.nodes1 = self.get_node_assignment(comms1)
+        self.write_files(comms1, comms2)
+        res = self.execute_cpp()
+        self.results = self.get_results(res)
+        """self.nodes1 = self.get_node_assignment(comms1)
         self.nodes2 = self.get_node_assignment(comms2)
         self.nodes = list(set().union([node for i, com in comms2.iteritems() for node in com],
-                                      [node for i, com in comms1.iteritems() for node in com]))
+                                      [node for i, com in comms1.iteritems() for node in com]))"""
+
+    def write_files(self, comms1, comms2):
+        with open('./nmi/file1.txt', 'w') as fp:
+            for _, comm in comms1.iteritems():
+                for node in comm:
+                    fp.write(str(node))
+                    fp.write(" ")
+                fp.write("\n")
+        with open('./nmi/file2.txt', 'w') as fp:
+            for _, comm in comms2.iteritems():
+                for node in comm:
+                    fp.write(str(node))
+                    fp.write(" ")
+                fp.write("\n")
 
     def get_node_assignment(self,comms):
         """
@@ -23,3 +44,32 @@ class NMI:
                 except KeyError:
                     nodes[node] = [i]
         return nodes
+
+    def execute_cpp(self):
+        p = Popen(['./nmi/onmi ./nmi/file1.txt ./nmi/file2.txt'], shell=True, stdout=PIPE, stdin=PIPE)
+        result = []
+        for ii in range(4):
+            value = str(ii) + '\n'
+            #value = bytes(value, 'UTF-8')  # Needed in Python 3.
+            try:
+                p.stdin.write(value)
+            except IOError:
+                pass
+            p.stdin.flush()
+            result.append(p.stdout.readline().strip())
+        return result
+
+    def get_results(self, results):
+        res = {}
+        for line in results:
+            if line.split(":")[1] == "":
+                continue
+            res[line.split(":")[0]] = float(line.split(":")[1].strip())
+        return res
+
+if __name__=='__main__':
+    comms1 = {1: [5, 6, 7], 2: [3, 4, 5], 3: [6, 7, 8]}
+    comms2 = {1: [5, 6, 7], 2: [3, 4, 6], 3: [6, 7, 8]}
+    nmi = NMI(comms1, comms2).results
+    print nmi
+
